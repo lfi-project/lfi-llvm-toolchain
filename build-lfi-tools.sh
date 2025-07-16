869b4eea9f5182d5062430f2f021526a5e3bb30c
Zachary Yedidia <zyedidia@gmail.com>
2025-07-16 12:54:45 -0700

    Force rebuild lfi tools each time

--- build-lfi-tools.sh
+++ build-lfi-tools.sh
@@ -6,18 +6,21 @@ PREFIX=$1
 MARCH=$2
 
 cd lfi-runtime
+rm -rf build
 meson setup build --cross-file toolchains/$MARCH-linux-clang.meson --libdir=lib --prefix=$PREFIX
 cd build
 ninja install
 cd ../..
 
 cd lfi-verifier
+rm -rf build
 meson setup build --libdir=lib --prefix=$PREFIX
 cd build
 ninja install
 cd ../..
 
 cd lfi-rewriter
+rm -rf build
 meson setup build --libdir=lib --prefix=$PREFIX
 cd build
 ninja install
